74e01ef5724e5cf131e8a0b6d4609a9192fdeff5
Mohamed Riyaz <86571297+riyazamra1@users.noreply.github.com>
2026-09-14 13:27:55 +0530

    feat: configure RSS Downloader Android app

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.riyaz.rssdownloader"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.riyaz.rssdownloader"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.webkit:webkit:1.12.1")
+}
