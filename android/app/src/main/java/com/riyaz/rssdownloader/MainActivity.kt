5267171f50f6f412f49ba117ada6c57f02afad72
Mohamed Riyaz <86571297+riyazamra1@users.noreply.github.com>
2026-09-14 13:28:14 +0530

    feat: package RSS Downloader UI in native Android shell

---
+++ android/app/src/main/java/com/riyaz/rssdownloader/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.riyaz.rssdownloader
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.mediaPlaybackRequiresUserGesture = false
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("file:///android_asset/ui/index.html")
+        setContentView(webView)
+    }
+}
